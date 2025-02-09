4213a555da8433e15505122554f0041a6d13f654
Adam CHELLALI <118626434+ChellAdam@users.noreply.github.com>
2025-02-09 18:32:24 +0100

    Add files via upload

---
+++ js/Obstacle.js
@@ -0,0 +1,19 @@
+import ObjectGraphique from "./ObjectGraphique.js";
+
+export default class Obstacle extends ObjectGraphique {
+    constructor(x, y, w, h, couleur) {
+        super(x, y, w, h, couleur);
+    }
+
+    draw(ctx) {
+        ctx.save();
+        ctx.fillStyle = this.couleur;
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+        ctx.restore();
+    }
+
+    destroy() {
+        // Suppression explicite des références et des éventuels écouteurs d'événements
+        console.log(`${this.constructor.name} détruit.`);
+    }
+}
